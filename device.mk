7ba7805e58c8a504463072983add0739986d2b82
Anton Hansson <hansson@google.com>
2018-10-22 15:33:51 +0100

    dipper: Move libnfc-nci.conf to /product
    Device-specific files belong off /system.
    
    Bug: 117878616
    Test: make
    Change-Id: Iddad62c178c553e4d628deb4509ea54451b8ab4f
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -89,7 +89,8 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:system/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # OTA Updates
