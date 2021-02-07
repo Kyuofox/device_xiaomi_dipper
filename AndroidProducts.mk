a4c9bce470ed4ea56b8abde64f9f77266a02bc34
KyuoFoxHuyu <88403766@qq.com>
2021-02-07 07:16:58 +0000

    dipper: Dot-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yaap_dipper.mk
+    $(LOCAL_DIR)/dot_dipper.mk
 
 COMMON_LUNCH_CHOICES := \
-yaap_dipper-user \
-yaap_dipper-userdebug \
-yaap_dipper-eng
+dot_dipper-user \
+dot_dipper-userdebug \
+dot_dipper-eng
--- dot_dipper.mk
+++ dot_dipper.mk
@@ -6,13 +6,13 @@
 
 $(call inherit-product, device/xiaomi/dipper/device.mk)
 
-# Inherit some common YAAP stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+# Inherit some common Dot stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 scr_resolution := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := yaap_dipper
+PRODUCT_NAME := dot_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
