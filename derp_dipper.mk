cf90d5f83ba02a5ec6a2f50d11356202f491b281
KyuoFoxHuyu <88403766@qq.com>
2021-08-24 22:39:37 +0800

    dipper: Bring-up DerpFest
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- derp_dipper.mk
+++ derp_dipper.mk
@@ -6,15 +6,15 @@
 
 $(call inherit-product, device/xiaomi/dipper/device.mk)
 
-# Inherit some DotOS stuff.
-$(call inherit-product, vendor/dot/config/common.mk)
+# Inherit some DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common.mk)
 
 # Inherit some common Lineage stuff.
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := dot_dipper
+PRODUCT_NAME := derp_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
